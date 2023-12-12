4a1413acd5ff1a831bdbe8c56b82600742233fab
Nathan <93161331+nathansmith117@users.noreply.github.com>
2023-12-12 08:03:28 -0700

    First commit

---
+++ RaccoonConvert/Resources/RaccoonConvertApp.swift
@@ -0,0 +1,20 @@
+//
+//  RaccoonConvertApp.swift
+//  RaccoonConvert
+//
+//  Created by Smith, Nathanael on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct RaccoonConvertApp: App
+{
+    var body: some Scene
+    {
+        WindowGroup
+        {
+            RaccoonConvertView()
+        }
+    }
+}
---
+++ RaccoonConvert/ViewController/RaccoonConvertView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  RaccoonConvert
+//
+//  Created by Smith, Nathanael on 12/12/23.
+//
+
+import SwiftUI
+
+struct RaccoonConvertView: View
+{
+    var body: some View
+    {
+        VStack
+        {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview
+{
+    RaccoonConvertView()
+}
