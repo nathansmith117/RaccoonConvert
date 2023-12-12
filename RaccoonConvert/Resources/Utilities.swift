fdd612f3869998a0079b44fafea755833f58031a
Nathan <93161331+nathansmith117@users.noreply.github.com>
2023-12-12 08:53:54 -0700

    Added decimal converting stuff

---
+++ RaccoonConvert/Resources/Utilities.swift
@@ -0,0 +1,25 @@
+//
+//  Utilities.swift
+//  RaccoonConvert
+//
+//  Created by Smith, Nathanael on 12/12/23.
+//
+
+import Foundation
+
+// Decimal, hex, binary, oct.
+
+// Decimal.
+func toDecimal(value: Int) -> String?
+{
+    let converted: String? = String(value)
+    return converted
+}
+
+func fromDecimal(value: String) -> Int?
+{
+    let converted: Int? = Int(value)
+    return converted
+}
+
+
