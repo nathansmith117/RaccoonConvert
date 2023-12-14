337c9b46e9f148db7f13f263b5c8be1835f32730
Nathan <93161331+nathansmith117@users.noreply.github.com>
2023-12-14 09:45:42 -0700

    Shooter moves

--- RaccoonConvert/Scenes/CoonShootScene.swift
+++ RaccoonConvert/Scenes/CoonShootScene.swift
@@ -9,10 +9,50 @@ import SpriteKit
 
 class CoonShootScene: SKScene
 {
-    private let shootNode: SKSpriteNode = SKSpriteNode(color: UIColor.blue, size: CGSize(width: 100, height: 100))
+    private let shootNode: SKSpriteNode = SKSpriteNode(color: UIColor.white, size: CGSize(width: 20, height: 20))
     
     override func didMove(to view: SKView) -> Void
     {
         addChild(shootNode)
+    }
+    
+    private func moveShootNode(to location: CGPoint)
+    {
+        if location.x >= 0 && location.y >= 0 && location.x <= size.width && location.y <= size.height
+        {
+            shootNode.position = location
+        }
+    }
+    
+    override func touchesBegan(_ touches : Set<UITouch>, with event : UIEvent?) -> Void
+    {
+        guard let touch = touches.first
+        else { return }
+        
+        moveShootNode(to: touch.location(in: self))
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        guard let touch = touches.first
+        else { return }
+        
+        moveShootNode(to: touch.location(in: self))
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        guard let touch = touches.first
+        else { return }
+        
+        moveShootNode(to: touch.location(in: self))
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        guard let touch = touches.first
+        else { return }
+        
+        moveShootNode(to: touch.location(in: self))
     }
 }
